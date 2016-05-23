00202824fe2c2487a2ff15b28cb7e88c3d8eabf3
javiier19 <luijaniet@gmail.com>
2016-05-22 22:39:43 -0500

    add New Punto_venta

--- js/controllers/puntoVentaController.js
+++ js/controllers/puntoVentaController.js
@@ -1,6 +1,6 @@
 var app = angular.module('inprovec');
 
-app.controller('PuntoVentaIndexCtrl', function ($mdEditDialog, $q, $scope, listaPuntoVenta, $mdDialog) {
+app.controller('PuntoVentaIndexCtrl', function ($mdEditDialog, $q, $scope, listaPuntoVenta, $mdDialog ,$timeout) {
 
     $scope.query = {
         order: 'nombre',
@@ -10,4 +10,38 @@ app.controller('PuntoVentaIndexCtrl', function ($mdEditDialog, $q, $scope, lista
 
     $scope.PuntoVenta = listaPuntoVenta.query();
 
+    $scope.user = null;
+    $scope.users = null;
+    
+    $scope.loadUsers = function() {
+        // Use timeout to simulate a 650ms request.
+        return $timeout(function() {
+            $scope.users =  $scope.PuntoVenta.ciudad.departamento.nombre;
+        }, 650);
+    };
+    $scope.showAdd = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: "templates/Vendedor/_formVend.html",
+            targetEvent: ev
+        });
+    };
+
+    var DialogController = function ($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            listaVendedor.save($scope.vendedor,function (data) {
+                $scope.vendedor = {};
+                console.log(data)
+            },function (err) {
+                alert(err)
+            });
+            $mdDialog.hide(answer);
+        };
+    };
 });
