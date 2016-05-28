00f766c31dcadc4aaadbbf064ea1ac9b54f082d7
javiier19 <luijaniet@gmail.com>
2016-05-28 10:59:15 -0500

    Edit Controller Vendedor

--- js/controllers/vendedorController.js
+++ js/controllers/vendedorController.js
@@ -1,6 +1,6 @@
 var app = angular.module('inprovec');
 
-app.controller('VendedorIndexCtrl', function ($mdEditDialog, $q, $scope, listaVendedor, $mdDialog) {
+app.controller('VendedorIndexCtrl', function ($mdEditDialog, $q, $scope, listaVendedor) {
 
     $scope.selected = [];
 
@@ -17,30 +17,15 @@ app.controller('VendedorIndexCtrl', function ($mdEditDialog, $q, $scope, listaVe
 
     $scope.vendedor = listaVendedor.query();
 
-    $scope.showAdd = function(ev) {
-        $mdDialog.show({
-            controller: DialogController,
-            templateUrl: "templates/Vendedor/_formVend.html",
-            targetEvent: ev
-        });
-    };
+    $scope.guardarVendedor = function () {
+        listaVendedor.save($scope.vendedor,function (data) {
+            console.log(vendedor);
+            $scope.vendedor = {}
+        },function (err) {
+            alert("ojo la estas cagando")
+        })
 
-    var DialogController = function ($scope, $mdDialog) {
-        $scope.hide = function() {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function() {
-            $mdDialog.cancel();
-        };
-        $scope.answer = function(answer) {
-            listaVendedor.save($scope.vendedor,function (data) {
-                $scope.vendedor = {};
-                console.log(data)
-            },function (err) {
-                alert(err)
-            });
-            $mdDialog.hide(answer);
-        };
+
     };
 });
 
